bcd62dd4027ecaee1ea4e87ba262fdb9299d2a38
Mariam <mariampataraia9@gmail.com>
2023-03-16 21:30:17 +0400

    add some js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-// Your JavaScript code
-const buttons = document.querySelectorAll('.green-button');
+
+const buttons = document.querySelectorAll('.tip_button');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         // Set the background color of the clicked button to red
-        button.style.backgroundColor = 'red';
+        button.style.backgroundColor = 'rgb(16, 187, 150)';
 
         // Set the background color of all the other buttons back to green
         buttons.forEach(otherButton => {
@@ -14,3 +14,51 @@ buttons.forEach(button => {
         });
     });
 });
+
+
+
+let input = document.getElementById("bill");
+let five = document.getElementById("five");
+let ten = document.getElementById("ten");
+let fifteen = document.getElementById("fifteen");
+let twentyfive = document.getElementById("twentyfive");
+let fifty = document.getElementById("fifty");
+let tip = document.getElementById("tip");
+let total = document.getElementById("total");
+let people = document.getElementById("people");
+
+
+five.addEventListener("click", () => {
+    let number = parseFloat(input.value);
+    let five = (5 / 100) * number;
+    tip.innerText = `$ ${five}`;
+    total.innerHTML = `$ ${five + number}`;
+
+});
+ten.addEventListener("click", () => {
+    let number = parseFloat(input.value);
+    let ten = (10 / 100) * number;
+    tip.innerText = `$ ${ten}`;
+    total.innerHTML = `$ ${ten + number}`;
+});
+fifteen.addEventListener("click", () => {
+    let number = parseFloat(input.value);
+    let fifteen = (15 / 100) * number;
+    tip.innerText = `$ ${fifteen}`;
+    total.innerHTML = `$ ${fifteen + number}`;
+});
+twentyfive.addEventListener("click", () => {
+    let number = parseFloat(input.value);
+    let twentyfive = (25 / 100) * number;
+    tip.innerText = `$ ${twentyfive}`;
+    total.innerHTML = `$ ${twentyfive + number}`;
+});
+fifty.addEventListener("click", () => {
+    let number = parseFloat(input.value);
+    let fifty = (50 / 100) * number;
+    tip.innerText = `$ ${fifty}`;
+    total.innerHTML = `$ ${fifty + number}`;
+});
+
+
+
