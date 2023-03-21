2e905c975df4d57857fcab6092cae9a75acf8cea
Mariam <mariampataraia9@gmail.com>
2023-03-22 00:57:07 +0400

    add experimental changes

--- script/script.js
+++ script/script.js
@@ -151,19 +151,36 @@ custom.addEventListener("click", () => {
 
 let button = document.getElementById('custom');
 
-button.addEventListener('click', () => {
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.style.width = '5.88vw';
-    input.setAttribute('id', 'customInput');
-    input.classList.add('input');
-    input.classList.add('inputtt');
-    input.addEventListener('blur', () => {
-        button.innerText = input.value;
+// button.addEventListener('click', () => {
+//     const input = document.createElement('input');
+//     input.type = 'text';
+//     input.style.width = '5.88vw';
+//     input.setAttribute('id', 'customInput');
+//     input.classList.add('input');
+//     input.classList.add('inputtt');
+//     input.addEventListener('blur', () => {
+//         button.innerText = input.value;
 
-    });
-    button.replaceWith(input);
-    input.focus();
+//     });
+//     button.replaceWith(input);
+//     input.focus();
+
+// });
+
+
+let customInput = document.getElementById("customInput");
+
+button.addEventListener('click', () => {
+    button.style.display = "none";
+    customInput.style.display = "block";
+
+    // input.addEventListener('blur', () => {
+    //     button.innerText = input.value;
+
+    // });
+    // button.replaceWith(customInput);
+    customInput.focus();
+    // button.focus();
 
 });
 
@@ -171,15 +188,6 @@ button.addEventListener('click', () => {
 
 
 
-// button.addEventListener("click", () => {
-//     let amount = Math.round(input.value);
-//     let value=button.value
-//     lastTipValue = (50 / 100) * amount;
-//     lastTotalValue = lastTipValue + parseFloat(input.value);
-//     tip.innerText = `$ ${lastTipValue / people.value}`;
-//     total.innerHTML = `$ ${lastTotalValue / people.value}`;
-//     activeBtn = 0.5;
-// });
 
 
 
@@ -187,13 +195,5 @@ button.addEventListener('click', () => {
 
 
 
-// fifty.addEventListener("click", () => {
-//     let number = Math.round(input.value);
-//     let fifty = (50 / 100) * number;
-//     tip.innerText = `$ ${fifty / people.value}`;
-//     total.innerHTML = `$ ${(fifty + number) / people.value}`;
-//     lastTipValue = fifty;
-//     lastTotalValue = fifty + number;
-//     actoveBtn = fifty;
-// });
+
 
